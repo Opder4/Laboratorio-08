b8cb9161a6dccdecf2213137c2b36edeb439e39e
Emanuel Ramírez <er6348100@gmail.com>
2026-04-27 11:19:08 -0600

    Fin ej 3

--- Ejercicio 2/Program.cs
+++ Ejercicio 2/Program.cs
@@ -1,1 +1,90 @@
-﻿
+﻿int numproductos = 0;
+int numdias = 0;
+int valor = 0;
+int filaver = 0;
+
+void Error()
+{
+    Console.WriteLine();
+    Console.WriteLine("Dato no válido");
+    Console.WriteLine();
+}
+
+void Limpiar()
+{
+    Console.WriteLine();
+    Console.WriteLine("Presione ENTER para continuar");
+    Console.WriteLine();
+    Console.ReadLine();
+    Console.Clear();
+}
+
+void PedirValores()
+{
+    Console.WriteLine("Ingrese el número de produtos: ");
+    Console.WriteLine();
+    while (!int.TryParse(Console.ReadLine(), out numproductos) || numproductos <= 0)
+    {
+        Error();
+    }
+    Console.WriteLine();
+    Console.WriteLine("Ingrese el número de días: ");
+    Console.WriteLine();
+    while (!int.TryParse(Console.ReadLine(), out numdias) || numdias <= 0)
+    {
+        Error();
+    }
+    Limpiar();
+}
+
+
+void IngresarCantidadVentas(int[,] cantvent)
+{
+    for (int i = 0; i < numproductos; i++)
+    {
+        for (int j = 0; j < numdias; j++)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Ingrese la primera cantidad vendidad");
+            Console.WriteLine();
+            while (!int.TryParse(Console.ReadLine(), out cantvent[i, j]))
+            {
+                Error();
+            }
+            Limpiar();
+        }
+    }
+}
+
+void PedirFila()
+{
+    Console.WriteLine();
+    Console.WriteLine("Ingrese la fila del producto que desea ver las cantidades de ventas");
+    Console.WriteLine();
+    while (!int.TryParse(Console.ReadLine(),out filaver) || filaver < 0 || filaver>=numproductos)
+    {
+        Error();
+    }
+    Limpiar();
+}
+
+void MostrarCantidadVentas(int[,] cant)
+{
+    Console.WriteLine();
+    Console.WriteLine("La cantidad de ventas de ese producto es de esta forma");
+    Console.WriteLine();
+    for (int i = 0; i<numdias;i++)
+    {
+        Console.Write(cant[filaver, i] + " ");
+    }
+    Limpiar();
+}
+
+PedirValores();
+int[,] cantidadventas = new int[numproductos, numdias];
+IngresarCantidadVentas(cantidadventas);
+PedirFila();
+MostrarCantidadVentas(cantidadventas);
+
+
+
---
+++ Ejercicio 3/Program.cs
@@ -0,0 +1,76 @@
+﻿int numapartamentos = 0;
+int numdias = 0;
+double consum = 0;
+double total = 0;
+
+void Error()
+{
+    Console.WriteLine();
+    Console.WriteLine("Dato no válido");
+    Console.WriteLine();
+}
+
+void Limpiar()
+{
+    Console.WriteLine();
+    Console.WriteLine("Presione ENTER para continuar");
+    Console.WriteLine();
+    Console.ReadLine();
+    Console.Clear();
+}
+
+void PedirValores()
+{
+    Console.WriteLine("Ingrese el número de apartamentos: ");
+    Console.WriteLine();
+    while (!int.TryParse(Console.ReadLine(), out numapartamentos) || numapartamentos <= 0)
+    {
+        Error();
+    }
+    Console.WriteLine();
+    Console.WriteLine("Ingrese el número de días: ");
+    Console.WriteLine();
+    while (!int.TryParse(Console.ReadLine(), out numdias) || numdias <= 0)
+    {
+        Error();
+    }
+    Limpiar();
+}
+
+
+void IngresarConsumo(double[,] consum)
+{
+    for (int i = 0; i < numapartamentos; i++)
+    {
+        for (int j = 0; j < numdias; j++)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Ingrese el consumo: ");
+            Console.WriteLine();
+            while (!double.TryParse(Console.ReadLine(), out consum[i, j]))
+            {
+                Error();
+            }
+            Limpiar();
+        }
+    }
+}
+
+double TotalConsumo(double[,] totale)
+{
+    for (int i = 0; i < numapartamentos; i++)
+    {
+        for (int j = 0; j < numdias; j++)
+        {
+            consum += totale[i, j];
+        }
+    }
+    return consum;
+}
+
+PedirValores();
+double[,] consumo = new double[numapartamentos, numdias];
+IngresarConsumo(consumo);
+Console.WriteLine();
+Console.WriteLine($"EL total de consumo es de {TotalConsumo(consumo)}");
+Console.WriteLine();
