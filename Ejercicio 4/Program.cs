a33f43f90f697c76a4c3365be7a738e1efce0c69
Emanuel Ramírez <er6348100@gmail.com>
2026-04-27 11:44:42 -0600

    No se concluyó el laboratorio

--- Ejercicio 4/Program.cs
+++ Ejercicio 4/Program.cs
@@ -1,1 +1,79 @@
-﻿
+﻿int numestudiantes = 0;
+int numevaluaciones = 0;
+int calificacion = 0;
+int mayor = int.MinValue;
+
+void Error()
+{
+    Console.WriteLine();
+    Console.WriteLine("Dato no válido");
+    Console.WriteLine();
+}
+
+void Limpiar()
+{
+    Console.WriteLine();
+    Console.WriteLine("Presione ENTER para continuar");
+    Console.WriteLine();
+    Console.ReadLine();
+    Console.Clear();
+}
+
+void PedirValores()
+{
+    Console.WriteLine("Ingrese el número de estudiantes: ");
+    Console.WriteLine();
+    while (!int.TryParse(Console.ReadLine(), out numestudiantes) || numestudiantes <= 0)
+    {
+        Error();
+    }
+    Console.WriteLine();
+    Console.WriteLine("Ingrese el número de evaluaciones: ");
+    Console.WriteLine();
+    while (!int.TryParse(Console.ReadLine(), out numevaluaciones) || numevaluaciones <= 0)
+    {
+        Error();
+    }
+    Limpiar();
+}
+
+
+void IngresarCalificaciones(int[,] cali)
+{
+    for (int i = 0; i < numestudiantes; i++)
+    {
+        for (int j = 0; j < numevaluaciones; j++)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Ingrese la calificación: ");
+            Console.WriteLine();
+            while (!int.TryParse(Console.ReadLine(), out cali[i, j]))
+            {
+                Error();
+            }
+            Limpiar();
+        }
+    }
+}
+
+int BuscarMayor(int[,] mayo)
+{
+    for (int i = 0; i < numestudiantes; i++)
+    {
+        for (int j = 0; j < numevaluaciones; j++)
+        {
+            if (mayo[i,j]>mayor)
+            {
+                mayor=mayo[i,j];
+            }
+        }
+    }
+    return mayor;
+}
+
+PedirValores();
+int[,] calificaciones = new int[numestudiantes, numevaluaciones];
+IngresarCalificaciones(calificaciones);
+Console.WriteLine();
+Console.WriteLine($"La mayor calificación fue de {BuscarMayor(calificaciones)} puntos");
+Console.WriteLine();
---
+++ Ejercicio 5/Program.cs
@@ -0,0 +1,94 @@
+﻿int numproductos = 0;
+int numcategorias = 0;
+int valorproducto = 0;
+int buscarvalor = 0;
+int encontrado = 0;
+int contencontrado = 0;
+int fila = 0;
+int columna = 0;
+
+void Error()
+{
+    Console.WriteLine();
+    Console.WriteLine("Dato no válido");
+    Console.WriteLine();
+}
+
+void Limpiar()
+{
+    Console.WriteLine();
+    Console.WriteLine("Presione ENTER para continuar");
+    Console.WriteLine();
+    Console.ReadLine();
+    Console.Clear();
+}
+
+void PedirValores()
+{
+    Console.WriteLine("Ingrese el número de estudiantes: ");
+    Console.WriteLine();
+    while (!int.TryParse(Console.ReadLine(), out numproductos) || numproductos <= 0)
+    {
+        Error();
+    }
+    Console.WriteLine();
+    Console.WriteLine("Ingrese el número de evaluaciones: ");
+    Console.WriteLine();
+    while (!int.TryParse(Console.ReadLine(), out numcategorias) || numcategorias <= 0)
+    {
+        Error();
+    }
+    Limpiar();
+}
+
+
+void IngresarValor(int[,] cali)
+{
+    for (int i = 0; i < numproductos; i++)
+    {
+        for (int j = 0; j < numcategorias; j++)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Ingrese el valor : ");
+            Console.WriteLine();
+            while (!int.TryParse(Console.ReadLine(), out cali[i, j]))
+            {
+                Error();
+            }
+            Limpiar();
+        }
+    }
+}
+
+void ValorABuscar()
+{
+    Console.WriteLine();
+    Console.WriteLine("Ingrese el valor a buscar en el inventario");
+    Console.WriteLine();
+    while (!int.TryParse(Console.ReadLine(), out buscarvalor))
+    {
+        Error();
+    }
+    Limpiar();
+}
+
+void Buscar(int[,] pro)
+{
+    for (int i = 0; i < numproductos;i++)
+    {
+        for (int j = 0; j < numcategorias; j++)
+        {
+            if (pro[i,j] == buscarvalor)
+            {
+
+            }
+        }
+    }
+}
+
+PedirValores();
+int[,] productos = new int[numproductos, numcategorias];
+IngresarValor(productos);
+ValorABuscar();
+
+
